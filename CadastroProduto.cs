dd4a33b57fa8029cdd24312727f21f608f069a6a
ThePlayer3K <miyasakiguilherme@gmail.com>
2025-11-30 23:35:36 -0300

    Requisito de 1 throw cumprido

--- CadastroProduto.cs
+++ CadastroProduto.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -31,6 +32,33 @@ namespace QuickShop
 
         private void button1_Click(object sender, EventArgs e)
         {
+            try
+            {
+                int validacao = int.Parse(txtCod.Text);
+            }
+            catch
+            {
+                MessageBox.Show("ID inválido! Tente novamente.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                int validacao = int.Parse(txtQtd.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Quantidade em estoque inválida! Tente novamente.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                decimal validacao = decimal.Parse(txtPreco.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Preço inválido! Tente novamente.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
             if (txtCod.Text == "" || txtNome.Text == "" || txtPreco.Text == "" || txtQtd.Text == "")
             {
                 MessageBox.Show("Todos os campos devem ser preenchidos", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
@@ -42,19 +70,37 @@ namespace QuickShop
                     MessageBox.Show("A quantidade em estoque não pode ser negativa!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     return;
                 }
-                foreach (int id in ids_Produtos)
+                try
                 {
-                    if (txtCod.Text == id.ToString())
+                    foreach (int id in ids_Produtos)
                     {
-                        MessageBox.Show("Já existe um produto cadastrado (ou que foi removido anteriormente) com esse código!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
+
+                        if (txtCod.Text == id.ToString())
+                        {
+                            string ativo = "SELECT ativo FROM produtos WHERE id = @id";
+                            MySqlCommand buscarStatus = new MySqlCommand(ativo, Program.conexao);
+                            buscarStatus.Parameters.AddWithValue("@id", id);
+                            bool status = (bool)buscarStatus.ExecuteScalar();
+                            if (status)
+                            {
+                                throw new Exception("Um produto com esse id já existe! Tente novamente com outro id.");
+                            }
+                            else
+                            {
+                                throw new Exception("Um produto anteriormente deletado portava esse id. Será necessário selecionar outro id para este novo produto.");
+                            }
+                        }
+
                     }
+                    Produto novoProduto = new Produto(int.Parse(txtCod.Text), txtNome.Text, decimal.Parse(txtPreco.Text), int.Parse(txtQtd.Text));
+                    Program.produtos.Add(novoProduto);
+                    novoProduto.adicionarProduto();
+                    MessageBox.Show("Produto cadastrado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Hide();
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
-                Produto novoProduto = new Produto(int.Parse(txtCod.Text), txtNome.Text, decimal.Parse(txtPreco.Text), int.Parse(txtQtd.Text));
-                Program.produtos.Add(novoProduto);
-                novoProduto.adicionarProduto(); 
-                MessageBox.Show("Produto cadastrado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                this.Hide();
             }
         }
 
